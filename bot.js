6f2086a6ef2c19bf573a3fb397670d1a96b8d219
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-25 14:34:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('message', message => {
         if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط ل السيرفرات**').then(m => m.delete(5000));
       if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**`ADMINISTRATOR`أنت لا تملك خاصية **' );
         let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-        let copy = "ITZ Bot";
+        let copy = "walter.js#2002";
         let request = `Requested By ${message.author.username}`;
         if (!args) return message.reply('**اكتب رسالة البرودكاست**');message.channel.send(`**هل أنت متأكد من ارسال البرودكاست؟ \nالبرودكاست: ** \` ${args}\``).then(msg => { //walter.js#2002
         msg.react('✅')
